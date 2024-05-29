54009431aff9e1ddbd1d55fe0b895bcae83e6cdc
amalguduru <amalguduru@gmail.com>
2024-05-29 19:18:56 -0400

    Added register and login pages

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Credentials
+// Username: gadwalpasupati345@gmail.com
+// Password: 123456
+// Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NTY1YTE5OGFjNmYxYTY2NmFiMDBkMyIsImlhdCI6MTcxNjk0MDI4MywiZXhwIjoxNzE5NTMyMjgzfQ.LOuCxuwmcVSfYARrI06wO_7SraOxzpeIJx0uLWTQ7GQ
